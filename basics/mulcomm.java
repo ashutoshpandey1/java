bd233ae4b12b163c7be30ff6883722ec4d64ebf7
Ankur <ashutoshpandey9519@gmail.com>
2023-05-23 20:10:43 +0530

    In this file we discuss multiline comment

---
+++ basics/mulcomm.java
@@ -0,0 +1,12 @@
+class mulcomm
+{
+    public static void main(String args[])
+    {
+        /* this is
+         * multi
+         * line
+         * comment
+         */
+        System.out.println("this code for multiline comment");
+    }
+}
