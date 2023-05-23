a98995d70042ea8eb1e098a05e4a8c61bfb2c440
Ankur <ashutoshpandey9519@gmail.com>
2023-05-23 20:18:35 +0530

    In this file we discuss documentation comment

---
+++ basics/doccomm.java
@@ -0,0 +1,13 @@
+class doccomm
+{
+    public static void main(String args[])
+    {
+        /**
+         * this is
+         * documentation 
+         * commets
+         */
+        
+        System.out.println("this code for  documentation commets");
+    }
+}
